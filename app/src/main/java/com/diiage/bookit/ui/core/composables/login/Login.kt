fc5fd5b313955448c5c448c5a8a6444a5797acb0
mathixu.dev <mathieuroux71@gmail.com>
2023-09-20 13:39:03 +0200

    Login & Signup

---
+++ app/src/main/java/com/diiage/bookit/ui/core/composables/login/Login.kt
@@ -0,0 +1,93 @@
+package com.diiage.bookit.ui.core.composables.login
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.diiage.bookit.R
+
+@Composable
+fun Login() {
+    Box (
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color(0xFFFFFFFF))
+    ) {
+        Column {
+            LoginInformation()
+            LoginForm()
+        }
+    }
+}
+
+@Composable
+fun LoginInformation() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.login_image),
+            contentDescription = "Login informations",
+            contentScale = ContentScale.FillBounds
+        )
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(bottom = 9.dp)
+        ) {
+            Text(
+                text = "Bienvenue",
+                style = TextStyle(
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight(700),
+                    color = Color(0xFF000000),
+                    textAlign = TextAlign.Left
+                ),
+            )
+        }
+        Row (
+            modifier = Modifier.fillMaxWidth().padding(bottom = 35.dp)
+            ) {
+        Text(
+            text = "Connectez-vous pour continuer",
+            style = TextStyle (
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Normal,
+                color = Color(0xFF7A7A7A),
+                textAlign = TextAlign.Left
+                )
+            )
+        }
+    }
+}
+
+@Preview(name = "Login preview 1", showBackground = true)
+@Composable
+fun LoginPreview1() {
+    Box (
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color(0xFFFFFFFF))
+            .padding(40.dp)
+    ) {
+        Column {
+            Login()
+        }
+    }
+}
---
+++ app/src/main/java/com/diiage/bookit/ui/core/composables/signup/Signup.kt
@@ -0,0 +1,93 @@
+package com.diiage.bookit.ui.core.composables.signup
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.diiage.bookit.R
+
+@Composable
+fun SignUp() {
+    Box (
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color(0xFFFFFFFF))
+    ) {
+        Column {
+            SignUpInformation()
+            SignUpForm()
+        }
+    }
+}
+
+@Composable
+fun SignUpInformation() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.signup_ill),
+            contentDescription = "image description",
+            contentScale = ContentScale.FillBounds
+        )
+
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(bottom = 9.dp)
+        ) {
+            Text(
+                text = "Bienvenue !",
+                style = TextStyle(
+                    fontSize = 32.sp,
+                    fontWeight = FontWeight(700),
+                    color = Color(0xFF000000),
+                    textAlign = TextAlign.Center,
+                )
+            )
+        }
+        Row (
+            modifier = Modifier.fillMaxWidth().padding(bottom = 35.dp)
+        ) {
+            Text(
+                text = "Inscrivez-vous pour profiter de notre plateforme !",
+                style = TextStyle(
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight(400),
+                    color = Color(0xFF7A7A7A),
+                )
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SignUpPreview() {
+    Box (
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color(0xFFFFFFFF))
+            .padding(40.dp)
+    ) {
+        Column {
+            SignUp()
+        }
+    }
+}
