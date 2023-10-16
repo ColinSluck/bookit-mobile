377170cb227003541096ef139023e6023c39d963
Baptiste Renaudot <baptiste.renaudot@diiage.org>
2023-10-16 09:29:04 +0000

    Feature/create bookable link api

--- app/src/main/java/com/diiage/bookit/data/remote/repositories/BookableRepositoryImpl.kt
+++ app/src/main/java/com/diiage/bookit/data/remote/repositories/BookableRepositoryImpl.kt
@@ -7,30 +7,26 @@ import com.diiage.bookit.data.remote.queries.params.PaginatedParams
 import com.diiage.bookit.data.remote.responses.BookableResponse
 import com.diiage.bookit.data.remote.responses.PaginatedResponse
 import com.diiage.bookit.domain.models.Bookable
+import com.diiage.bookit.domain.models.CreateBookable
 import com.diiage.bookit.domain.models.Paginated
 import com.diiage.bookit.domain.models.Search
 import com.diiage.bookit.domain.repositories.BookableRepository
 import io.ktor.util.*
 
 class BookableRepositoryImpl(
-    private val apiAuth: ApiAuth,
-    private val mapper: BookableMapper
+    private val apiAuth: ApiAuth
 ) : BookableRepository{
     override suspend fun getAvailableBookables(page: Int): Paginated<Bookable> {
         val paginatedParams = PaginatedParams(page, 10)
 
-        val response = apiAuth.get<PaginatedResponse<BookableResponse>>(Url.BookingsAvailable.path, paginatedParams.toStringValues())
-
-        return mapper.toPaginedBookables(response)
+        return apiAuth.get<Paginated<Bookable>>(Url.BookingsAvailable.path, paginatedParams.toStringValues())
     }
 
     override suspend fun getBookable(id: Int): Bookable {
-        val response = apiAuth.get<BookableResponse>(Url.BookingsAvailable.path + "/$id")
-        return mapper.toBookable(response)
+        return apiAuth.get<Bookable>(Url.BookingsAvailable.path + "/$id")
     }
 
-    override suspend fun createBookable(bookable: Bookable): Bookable {
-       val response = apiAuth.post<BookableResponse>(Url.CreateBookable.path, mapper.toCreateBookable(bookable))
-        return mapper.toBookable(response)
+    override suspend fun createBookable(bookable: CreateBookable): Bookable {
+       return apiAuth.post<Bookable>(Url.CreateBookable.path, bookable)
     }
 }
